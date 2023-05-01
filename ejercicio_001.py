ade70f2e6b5607b422196d83267d37b7d60c1577
Pablov47 <121842157+Pablov47@users.noreply.github.com>
2023-04-30 18:32:24 -0600

    Update ejercicio_001.py
    Listo para revisar, el último ('La media de tu edad actual y tu edad en 20 años es') no supe como sacarlo. 
    Saludos.

--- ejercicio_001.py
+++ ejercicio_001.py
@@ -2,19 +2,28 @@
 name=input ('¿Cuál es tu nombre?')
 papellido=input('¿Cuál es tu primer apellido?')
 sapellido=input('¿Cuál es tu segundo apellido?')
-año_nacimiento=input('¿En qué año naciste?')
+año_nacimiento= int(input('¿En qué año naciste?'))
 mes_día=input('¿En qué mes y día naciste? (en el siguiente formato “mm-dd”)')
 año_actual=2022
-edad = año_actual - año_nacimiento
 
-nombre_completo = name + "" + papellido + sapellido
-print('Este es tu nombre completo en mayúsculas:' + nombre_completo)
-print('Este es tu nombre completo en minúsculas:' + nombre_completo)
-print('Esta es tu fecha de nacimiento “dd-mm-aaaa”:' +  mes_día + '-' + año_nacimiento)
-print('Esta es tu edad:'+  edad)
-print('Tu nombre completo tiene' + + 'vocales.')
-print('Tu nombre completo tiene' + + 'letras')
-print('¿Tu edad es un carácter de tipo número?'+ '__True__')
-print('¿Tu nombre completo es un carácter de tipo alfanumérico?'+ '__True__')
-print('Tu edad en 10 años será'+ edad + 10)
+nombre_completo = name + " " + papellido + " " + sapellido
+nombre_completo_mayus = nombre_completo.upper()
+print(f"Este es tu nombre completo en mayúsculas {nombre_completo_mayus}.")
+nombre_completo_minus = nombre_completo.lower()
+print(f'Este es tu nombre completo en minúsculas {nombre_completo_minus}.')
+fecha_nacimiento = mes_día + "-" + str(año_nacimiento)
+print(f"Esta es tu fecha de nacimiento '{fecha_nacimiento}'.")
+edad = año_actual - año_nacimiento
+print(f'Esta es tu edad: {edad}')
+vocales = "AEIOUaeiou"
+cant_vocales = sum([1 for letra in nombre_completo if letra in vocales])
+print(f'Tu nombre completo tiene {cant_vocales} vocales.')
+cant_letras = len(nombre_completo)
+print(f"Tu nombre completo tiene {cant_letras} letras.")
+es_entero = isinstance(edad, int)
+print(f'¿Tu edad es un carácter de tipo número? {es_entero}')
+es_alfanumerico = nombre_completo.isalnum()
+print(f'¿Tu nombre completo es un carácter de tipo alfanumérico? {es_alfanumerico}')
+edad_10_anos = edad + 10
+print(f'Tu edad en 10 años será {edad_10_anos}.')
 print('La media de tu edad actual y tu edad en 20 años es')
